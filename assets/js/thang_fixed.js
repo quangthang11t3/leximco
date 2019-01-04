56aaa3ca9ab13ed7065bb8045dbab572bd952fac
thang <thang@cshool.jp>
2019-01-04 19:20:35 +0900

    create site

---
+++ assets/js/thang_fixed.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+	$('.slick_fade').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear',
+	  autoplay: true,
+	  pauseOnHover: false,
+	  autoplaySpeed: 2500,
+	});
+
+});
